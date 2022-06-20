431e2c3adf0ca3652f17ff59610dbcf244c3e2e0
Sther-asr <86372495+Sther-asr@users.noreply.github.com>
2022-06-20 00:29:37 -0400

    Add files via upload

--- src/routes/ingresos.js
+++ src/routes/ingresos.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 /**
  * rutas peticiones 
  */
-import {registraIngreso, consultaIngresos, actualizarIngreso, eliminarIngreso} from "../controllers/3-registros_ingresos-controllers";
+import {registraIngreso, consultaIngresos, consultaIngresosRango, actualizarIngreso, eliminarIngreso} from "../controllers/3-registros_ingresos-controllers";
 import {sesionExistente} from "../assets/permisos-peticiones";
 const router = Router();
 const path = 'ingresos';
@@ -10,7 +10,8 @@ const path = 'ingresos';
  * Rutas
  */
 router.post(`/registro`, sesionExistente, registraIngreso);
-router.get(`/consulta`, sesionExistente, consultaIngresos);
+router.post(`/consulta`, sesionExistente, consultaIngresos);
+router.post(`/consulta/rango`, sesionExistente, consultaIngresosRango);
 router.put(`/actualizar`, sesionExistente, actualizarIngreso);
 router.delete(`/eliminar`, sesionExistente, eliminarIngreso);
 
