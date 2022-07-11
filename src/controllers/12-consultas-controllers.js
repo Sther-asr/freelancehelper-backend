7ac27e9635670fc6fb158a9e660151ed94e4a406
Sther-asr <86372495+Sther-asr@users.noreply.github.com>
2022-07-11 12:07:55 -0400

    Add files via upload

--- src/controllers/12-consultas-controllers.js
+++ src/controllers/12-consultas-controllers.js
@@ -9,20 +9,20 @@ const date = require("date-and-time");
 export const consultaTareasDiarias = async (peticion, respuesta) => {
   try {
     console.log("Realizando consulta diaria");
-    const date = require("date-and-time");
+    // const date = require("date-and-time");
     const idPersona = parseInt(peticion.body.idSession);
     const objetoConexion = await conexion();
-    const fecha = peticion.body.fechaFin;
-    let fechaAnterior = date.format(new Date(fecha), "YYYY-MM-DD");
+    // const fecha = peticion.body.fechaFin;
+    // let fechaAnterior = date.format(new Date(fecha), "YYYY-MM-DD");
 
-    let resultado;
-    let resultado2;
+    let resultado = [];
+    let resultado2 = [];
 
     //Recordatorios
     try {
-      resultado = await objetoConexion.query(
-        "SELECT r.idRecordatorio, r.descripcion, r.fechaInicio, r.fechaFin, r.estado, r.persona_idPersona FROM recordatorio r WHERE ( DATE(r.fechaFIN) BETWEEN ? AND ?) AND persona_idPersona = ?",
-        [fechaAnterior, peticion.body.fechaFin, idPersona]
+      [resultado] = await objetoConexion.query(
+        "SELECT r.idRecordatorio, r.descripcion, r.fechaInicio, r.fechaFin, r.estado, r.persona_idPersona FROM recordatorio r WHERE r.estado = 1 AND r.persona_idPersona = ? ORDER BY r.fechaFin ASC",
+        [idPersona]
       );
     } catch (e) {
       console.log("Error durante la consulta de Recordatorios\n" + e.message);
@@ -30,16 +30,16 @@ export const consultaTareasDiarias = async (peticion, respuesta) => {
 
     //Actividades
     try {
-      resultado2 = await objetoConexion.query(
-        "SELECT a.idActividad, a.descripcion, a.fechaInicio, a.fechaFin, a.estado, a.proyecto_idProyecto, p.idProyecto, p.persona_idPersona FROM actividad a JOIN proyecto p ON a.proyecto_idProyecto = p.idProyecto WHERE (DATE(a.fechaFIN) BETWEEN ? AND ?) AND p.persona_idPersona= ?",
-        [fechaAnterior, peticion.body.fechaFin, idPersona]
+      [resultado2] = await objetoConexion.query(
+        "SELECT a.idActividad, a.descripcion, a.fechaInicio, a.fechaFin, a.estado, a.proyecto_idProyecto, p.idProyecto, p.persona_idPersona FROM actividad a JOIN proyecto p ON a.proyecto_idProyecto = p.idProyecto WHERE a.estado = 1 AND persona_idPersona = ? ORDER BY a.fechaFin ASC",
+        [idPersona]
       );
     } catch (e) {
       console.log("Error durante la consulta de Actividades\n" + e.message);
     }
 
     //Juntar ambos arrays en arrayTareas
-    var arrayTareas = resultado[0].concat(resultado2[0]);
+    var arrayTareas = resultado.concat(resultado2);
 
     //Ordenar objetos del array por fechaFin
     arrayTareas.sort(function compare(a, b) {
@@ -49,25 +49,17 @@ export const consultaTareasDiarias = async (peticion, respuesta) => {
     });
 
     // Formatear campos fechaFin/fechaInicio por 'YYYY-MM-DD HH:mm:ss'
-    //Y eliminar tareas viejas terminadas
     for (let i = 0; i < arrayTareas.length; i++) {
       let fin = arrayTareas[i]["fechaFin"];
-      const finNuevo = date.format(fin, "YYYY-MM-DD HH:mm:ss");
+      const finNuevo = date.format(fin, "YYYY-MM-DD HH:mm");
 
       let inicio = arrayTareas[i]["fechaInicio"];
-      const inicioNuevo = date.format(inicio, "YYYY-MM-DD HH:mm:ss");
+      const inicioNuevo = date.format(inicio, "YYYY-MM-DD HH:mm");
 
       arrayTareas[i]["fechaFin"] = finNuevo;
       arrayTareas[i]["fechaInicio"] = inicioNuevo;
-
-      if (
-        date.format(new Date(arrayTareas[i]["fechaFin"]), "YYYY-MM-DD") <
-          fecha &&
-        arrayTareas[i]["estado"] == "Terminado"
-      ) {
-        arrayTareas.splice(i, 1);
-      }
     }
+
     respuesta.json(arrayTareas);
   } catch (e) {
     console.log("Error durante la consulta\n" + e.message);
@@ -178,29 +170,29 @@ export const consultaMovimientos = async (peticion, respuesta) => {
   }
 };
 
-export const consultaMontoTotalMovimientos = async (peticion, respuesta) => {
+export const consultaMontoTotalMovimientos = async (peticion, respuesta) =>{
   try {
     console.log("Realizando consulta monto total movimientos");
     const objetoConexion = await conexion();
     const idPersona = parseInt(peticion.body.idSession);
-    const [egresos] = await objetoConexion.query(
-      "SELECT round(SUM(monto),2) AS totalEgresos FROM registros_egresos WHERE (MONTH(fecha) = MONTH(?)) AND persona_idPersona = ?",
-      [peticion.body.fecha, idPersona]
+    const [egresos] = await objetoConexion.query("SELECT round(SUM(monto),2) AS totalEgresos FROM registros_egresos WHERE persona_idPersona = ?",
+                      [idPersona]
     );
-    const [ingresos] = await objetoConexion.query(
-      "SELECT round(SUM(monto),2) AS totalIngresos FROM registros_ingresos WHERE (MONTH(fecha) = MONTH(?)) AND persona_idPersona = ?",
-      [peticion.body.fecha, idPersona]
+    const [ingresos] = await objetoConexion.query("SELECT round(SUM(monto),2) AS totalIngresos FROM registros_ingresos WHERE persona_idPersona = ?",
+                      [idPersona]
     );
-    const saldo = (ingresos[0].totalIngresos - egresos[0].totalEgresos).toFixed(
-      2
+    const [egresosMes] = await objetoConexion.query("SELECT round(SUM(monto),2) AS totalEgresos FROM registros_egresos WHERE (MONTH(fecha) = MONTH(?)) AND persona_idPersona = ?",
+                      [peticion.body.fecha, idPersona]
     );
-    const ahorro = ((saldo / 100) * 10).toFixed(2);
-    respuesta.json({
-      totalEgresos: egresos[0].totalEgresos,
-      totalIngresos: ingresos[0].totalIngresos,
-      saldo: saldo,
-      ahorro: ahorro,
-    });
+    const [ingresosMes] = await objetoConexion.query("SELECT round(SUM(monto),2) AS totalIngresos FROM registros_ingresos WHERE (MONTH(fecha) = MONTH(?)) AND persona_idPersona = ?",
+                      [peticion.body.fecha, idPersona]
+    );
+    const saldo = (ingresos[0].totalIngresos - egresos[0].totalEgresos).toFixed(2);
+    let ahorro = ((saldo / 100) * 10 ).toFixed(2);
+    if(ahorro<=0){
+      ahorro = 0;
+    }
+    respuesta.json({"totalEgresos":egresosMes[0].totalEgresos, "totalIngresos":ingresosMes[0].totalIngresos, "saldo":saldo, "ahorro":ahorro});
   } catch (e) {
     console.log(
       "Error durante la consulta de totales de ingresos\n" + e.message
@@ -209,7 +201,7 @@ export const consultaMontoTotalMovimientos = async (peticion, respuesta) => {
       "Error durante la consulta de totales de ingresos": e.message,
     });
   }
-};
+}
 
 export const actualizarPerfil = async (peticion, respuesta) => {
   try {
@@ -254,3 +246,82 @@ export const actualizarPerfil = async (peticion, respuesta) => {
     respuesta.json({ "resultado" : false, "error": e.message }  );
   }
 };
+
+export const actualizarProyectoTerminado = async (peticion, respuesta, next) => {
+  try {
+    console.log(`Realizando consulta de actividades`);
+    const idPersona = parseInt(peticion.body.idSession);
+    const objetoConexion = await conexion();
+    console.log(peticion.body)
+    
+    const [numActividTerminada] = await objetoConexion.query("SELECT COUNT(*) AS noTerminadas FROM actividad WHERE proyecto_idProyecto = ? AND estado = ?",
+        [peticion.body.idProyecto, "Activo"]
+    );
+    console.log(numActividTerminada[0]);
+    // si hay actividades activas retornar y salir del controlador
+    if(numActividTerminada[0].noTerminadas !== 0){
+
+      const [proyectoActivo] = await objetoConexion.query("UPDATE proyecto SET estado = ? WHERE idProyecto = ?",["Activo", peticion.body.idProyecto]);
+
+      const [eliminarIngreso] = await objetoConexion.query("DELETE FROM registros_ingresos WHERE proyecto_idProyecto = ?", [peticion.body.idProyecto]);
+      
+      respuesta.json({"registro":false, "noTerminadas":numActividTerminada[0].noTerminadas});
+
+      return;
+    }
+
+    const [actualizaProyecto] = await objetoConexion.query("UPDATE proyecto SET estado = ? WHERE idProyecto = ?",
+          ["Terminado", peticion.body.idProyecto]
+    );
+    console.log('Filas actualizadas: '+actualizaProyecto.affectedRows);
+    //si no se pudo actualizar el estado retornar
+    if(actualizaProyecto.affectedRows === 0){
+      respuesta.json({"actualizarProyect":false, "resultado":actualizaProyecto});
+      return;
+    }
+    
+    // console.log({"registro":true});
+    // respuesta.json({"registro":true});
+
+    next();
+
+  } catch (e) {
+    console.log('Error actualizar estado de proyecto\n'+e.message);
+    respuesta.json({"tipo de Error":e.message});  
+  }
+}
+
+export const registroIngresoProyecto =  async (peticion, respuesta) => {
+  try {
+    console.log(`Realizando consulta de descripcion`);
+    const idPersona = parseInt(peticion.body.idSession);
+    const idProyecto = parseInt(peticion.body.idProyecto);
+    const objetoConexion = await conexion();
+    console.log(peticion.body)
+    // consulta si existe ingreso con esta id
+    const [ingresoSimilar] = await objetoConexion.query("SELECT COUNT(*) AS similar FROM registros_ingresos WHERE proyecto_idProyecto = ?",[idProyecto]);
+
+    if(ingresoSimilar[0].similar !== 0){
+      console.log("Existe un ingreso para este proyecto");
+      respuesta.json({"registro":false});
+      return;
+    }
+
+    const [resultado] = await objetoConexion.query("INSERT INTO registros_ingresos (motivo, monto, fecha, proyecto_idProyecto, persona_idPersona) VALUES ( (SELECT proyecto.descripcion FROM proyecto WHERE proyecto.idProyecto = ?), (SELECT proyecto.monto FROM proyecto WHERE proyecto.idProyecto = ?), ?, ?, ?)",
+        [idProyecto, idProyecto, peticion.body.fecha, idProyecto, idPersona]
+    );
+    console.log("Registro exitoso de ingreso de proyecto");
+    // si hay actividades activas retornar y salir del controlador
+    // if(numActividTerminada[0].noTerminadas !== 0){
+    //   respuesta.json({"registro":false, "noTerminadas":numActividTerminada[0].noTerminadas});
+    //   return;
+    // }
+    
+    // console.log({"registro":true});
+    respuesta.json({"registro":true, "resultado":resultado});
+
+  } catch (e) {
+    console.log('Error al registrar ingreso de proyecto\n'+e.message);
+    respuesta.json({"tipo de Error":e.message});  
+  }
+}
